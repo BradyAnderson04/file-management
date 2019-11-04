52463d02d3f912f97902d67d253a360a302957f6
Brady <brjoande@iu.edu>
2019-11-04 18:51:05 -0500

    untested version of code need to test

---
+++ fileCheck.py
@@ -0,0 +1,15 @@
+import os
+import pyudev
+
+# Run continously
+while True:
+    # detect usb plugin
+    context = pyudev.Context()
+    for device in context.list_devices(subsystem='block', DEVTYPE='partition'):
+        print(device.get('ID_FS_LABEL', 'unlabeled partition'))
+        # check file system to see how old last modified files are
+        for filename in os.listdir(device):           # iterate through all files in directories
+            info = os.stat(filename)
+            if( info.st_mtime > 2.628 * (10**9)):
+                # delete files
+                os.remove(filename)
